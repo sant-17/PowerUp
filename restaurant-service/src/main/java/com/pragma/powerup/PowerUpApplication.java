1b1355d77b592c828fe3c600d3eff2ae09eae21f
Santiago <santiago.garciah17@gmail.com>
2023-03-21 11:12:57 -0500

    Plazoleta service created

--- restaurant-service/src/main/java/com/pragma/powerup/PowerUpApplication.java
+++ restaurant-service/src/main/java/com/pragma/powerup/PowerUpApplication.java
@@ -3,9 +3,10 @@ package com.pragma.powerup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PowerUpApplication {
