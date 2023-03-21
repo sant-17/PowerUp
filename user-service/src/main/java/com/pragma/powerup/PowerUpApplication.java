d1500d8c1f442a07678a921c7d1460561cc3cd0e
Santiago <santiago.garciah17@gmail.com>
2023-03-21 11:05:43 -0500

    user-service-dev updated

--- user-service/src/main/java/com/pragma/powerup/PowerUpApplication.java
+++ user-service/src/main/java/com/pragma/powerup/PowerUpApplication.java
@@ -3,8 +3,11 @@ package com.pragma.powerup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
-
+@EnableEurekaClient
+@FeignClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PowerUpApplication {
 
