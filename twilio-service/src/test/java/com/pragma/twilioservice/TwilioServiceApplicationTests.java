0badf9a3c03a3e1ea279a226d2cc20ee18f5b029
Santiago <santiago.garciah17@gmail.com>
2023-03-28 09:56:18 -0500

    HU 14 Twilio

---
+++ twilio-service/src/test/java/com/pragma/twilioservice/TwilioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.twilioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
